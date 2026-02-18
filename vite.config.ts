efb5282ace764efac8cfd3a27f46de17e4baeadf
Kalpesh Pawar <129174049+kp154437@users.noreply.github.com>
2026-02-18 11:09:47 +0530

    Configure PWA with vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { VitePWA } from 'vite-plugin-pwa';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'KSP Digital | Admin',
+        short_name: 'KSP Admin',
+        description: 'Portfolio Admin Panel & Dashboard',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        scope: './',
+        start_url: './',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   base: './',
 })
